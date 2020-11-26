ff6a81d48827bd99e5593872dc76993d63ac3d95
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-11-26 17:23:25 -0400

    Basic setup for a Card deck used in an application

---
+++ Elixir_Phoenix/cards/lib/cards.ex
@@ -0,0 +1,23 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+end
