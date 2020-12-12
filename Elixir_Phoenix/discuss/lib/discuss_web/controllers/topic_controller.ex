ac4a9ddf3c52089bf55da27577d094ffe954fdea
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-12-12 15:17:37 -0400

    Modified controller/topic_controller.ex and templates/topic/new.html.eex

--- Elixir_Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Elixir_Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -24,7 +24,8 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
       {:ok, post} -> IO.inspect(post)
-      {:error, changeset} -> IO.inspect(changeset)
+      {:error, changeset} ->
+        render(conn,"new.html",changeset: changeset)
     end
   end
 end
